fb94a95999f866fce43ffa9a733e7f84a4c5186f
albyx9 <alessandrolabate96@hotmail.it>
2021-04-12 16:42:11 +0200

    2 files modified, 1 deleted

--- index.js
+++ index.js
@@ -2,13 +2,14 @@
 import "./style.css"; 
 
 var cityElems = document.getElementsByClassName("città");
-var bottone_media = document.getElementById("mediaprova");
-var numero_elementi = cityElems.length; //numero città
+var bottone_media = document.getElementById("media");
 var vettore_elementi = [];
 var req = [];
+
 for (let elem of cityElems ) {
   vettore_elementi.push(elem.innerHTML);
   console.log(elem.innerHTML);
+  elem.onclick = () => display(elem.innerHTML);
 }
 bottone_media.onclick = display_avg(vettore_elementi);
 ///////////////////////////////////
@@ -21,10 +22,10 @@ function display_avg (v){
         var dataObject = JSON.parse(request.response);
         var temp = dataObject.main.temp;
         media += dataObject.main.temp / v.length;
-        document.getElementById("risposta").innerHTML = 
+        document.getElementById("risposta_media").innerHTML = 
         "la media è di " + media + "gradi";
       }else{
-        document.getElementById("risposta").innerHTML = "errore";
+        document.getElementById("risposta_media").innerHTML = "errore";
       }
     }
     request.open("GET", "https://api.openweathermap.org/data/2.5/weather?APPID=d0fda39104b3c7c45fe031a5392964c1&units=metric&q=" +
@@ -33,3 +34,41 @@ function display_avg (v){
   }
 }
 // 6048b7faacmsh8840d0109f92483p15a7a2jsn51f760b208fc
+
+var cityElems = document.getElementsByClassName("città");
+for (let elem of cityElems ) {
+  //definisco una funzione arraow come proprieta del DOM
+  elem.onclick = () => display(elem.innerHTML);
+}
+
+// Funzione collegata ai bottoni
+// "window" necessario in StackBlitz, può essere
+// omesso altrimenti
+
+function display(city) {
+  var request = new XMLHttpRequest(); // Costruzione dell'oggetto "request"
+
+  // Funzione callback invocata quando la request termina
+  request.onload = function() {
+    // funzione definita arrow
+    if (request.status === 200) {
+      var dataObject = JSON.parse(request.response);
+      document.getElementById("risposta").innerHTML =
+        "A " + city + " ci sono " + dataObject.main.temp + " gradi";
+    } else {
+      document.getElementById("risposta").innerText = "Errore";
+    }
+  };
+
+//AJAX per acquisire dati di un servizio definito da terzi
+
+  // Applico il metodo "open"
+  request.open(
+    "GET",
+    "https://api.openweathermap.org/data/2.5/weather?APPID=d0fda39104b3c7c45fe031a5392964c1&units=metric&q=" +
+      city,
+    true
+  );
+  // Applico il metodo send (al termine chiamerà il callback "onload")
+  request.send();
+};
